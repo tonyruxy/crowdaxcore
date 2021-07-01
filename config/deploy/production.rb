1cd3cf76565db4ded4cee1f5c81759d13ce0b28d
Ortus Africa Capital <tech@ortusafrica.com>
2021-07-01 07:33:51 +0300

    udpated gemfile

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '54.190.123.225', user: 'deploy', roles: %w{app db web}
+server '34.221.241.181', user: 'deploy', roles: %w{app db web}
 
 
 # role-based syntax
